0c682fb3cc901a3794ba80c874779cabb01de479
SwiftShadowFox <60271020+SwiftShadowFox@users.noreply.github.com>
2021-01-04 10:32:13 -0800

    Created Draft Movement Script
    New Movement Script using WASD or Arrow Keys, though WASD recommended since the Sprint mechanic uses Left Shift to activate. Currently working on trying to get the camera to follow the player into different rooms using Cinemachine.

---
+++ HopeGameJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float speed; // Motion Speed
+    public float walk = 5f; // Walk Speed
+    public float sprint = 8f; // Sprint Speed
+    Vector2 movement = new Vector2();
+
+    void Update()
+    {
+        /*if (Input.GetKey(KeyCode.W)) // Move Up
+        {
+            movement.y += 1;
+        }
+        if (Input.GetKey(KeyCode.S)) // Move Down
+        {
+            movement.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.D)) // Move Right
+        {
+            movement.x += 1;
+        }
+        if (Input.GetKey(KeyCode.A)) // Move Left
+        {
+            movement.x -= 1;
+        }*/
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        //Sprint
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = sprint;
+        }
+        else
+        {
+            speed = walk;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().MovePosition(GetComponent<Rigidbody2D>().position + movement * speed * Time.fixedDeltaTime);
+    }
+}
