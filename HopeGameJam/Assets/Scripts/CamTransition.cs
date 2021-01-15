0daacdad0041fed273e05e76cbc604a7bcb02b9f
SwiftShadowFox <60271020+SwiftShadowFox@users.noreply.github.com>
2021-01-15 13:30:25 -0800

    Camera Transitions

---
+++ HopeGameJam/Assets/Scripts/CamTransition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamTransition : MonoBehaviour
+{
+    public GameObject cam;
+
+    //Switch when entering room
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") && !other.isTrigger)
+        {
+            cam.SetActive(true);
+        }
+    }
+    //Switch when leaving room
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") && !other.isTrigger)
+        {
+            cam.SetActive(false);
+        }
+    }
+}
