6f4777486c3779f7f362e144e74eb4337c78ee67
Remmie <emily@isimar.com>
2021-01-16 11:58:26 -0800

    Created a Light Controller
    Made a script that will allow you to shrink the radius of the light. Press F in my scene for buggy results.

---
+++ HopeGameJam/Assets/Scripts/LightController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine;
+using System;
+
+public class LightController : MonoBehaviour
+{
+    Light2D hopeLight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hopeLight = gameObject.GetComponent<Light2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.F))
+        {
+
+            StartCoroutine(DimLights());
+        }
+    }
+
+    private IEnumerator DimLights()
+    {
+        var radius = hopeLight.pointLightOuterRadius;
+        while (radius > 0)
+        {
+            radius -= 0.1f;
+            hopeLight.pointLightOuterRadius = radius;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
